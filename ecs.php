dd37c9348ecc9b2551ade43d70ba373c4e6ee548
TomasVotruba <tomas.vot@gmail.com>
2021-02-08 00:16:31 +0100

    add ecs

---
+++ ecs.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\EasyCodingStandard\ValueObject\Option;
+use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $parameters = $containerConfigurator->parameters();
+    $parameters->set(Option::PATHS, [
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ]);
+
+    $parameters->set(Option::SETS, [
+        // run and fix, one by one
+        SetList::COMMON,
+        SetList::PSR_12,
+    ]);
+};
